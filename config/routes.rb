bb5c66f9199cafc6a27735d716e185e942686bfb
James Wen <jrw2175@columbia.edu>
2014-01-13 23:10:44 -0500

    Initialized front-end assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 GroupUs::Application.routes.draw do
   get   '/login', :to => 'sessions#new', :as => :login
   get   '/logout' => 'sessions#destroy', :as => :logout
-  get   '/test' => 'sessions#test', :as => :test
   match "/auth", to: redirect("https://api.groupme.com/oauth/authorize?client_id=soF5LCewG9wfZaXyUlKFXFerIcmUTva75ZPpRRnnrVWUyErz"), as: :auth, via: :get
   get   '/auth/:provider/callback' => 'sessions#create'
   get   '/auth/failure', :to => 'sessions#failure'
