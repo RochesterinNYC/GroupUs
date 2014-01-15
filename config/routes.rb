3b3e6872bed1543a63e35b25a7b27a9a557cfd6d
James Wen <jrw2175@columbia.edu>
2014-01-15 00:49:00 -0500

    Reconfigured GroupMe API Key

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 GroupUs::Application.routes.draw do
   get   '/login', :to => 'sessions#new', as: :login
   get   '/logout' => 'sessions#destroy', as: :logout
-  get   "/auth", to: redirect("https://api.groupme.com/oauth/authorize?client_id=soF5LCewG9wfZaXyUlKFXFerIcmUTva75ZPpRRnnrVWUyErz"), as: :auth
+  get   "/auth", to: redirect("https://api.groupme.com/oauth/authorize?client_id=#{ENV['GROUPME_API_KEY']}"), as: :auth
   get   '/auth/:provider/callback' => 'sessions#create'
   get   '/auth/failure', :to => 'sessions#failure'
   
